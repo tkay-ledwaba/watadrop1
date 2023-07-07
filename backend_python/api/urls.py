a3bf356f75fc7d91402777bf8c4c43f7bc3f1942
philoxenic <philoxenic@gmail.com>
2023-07-07 11:58:42 +0200

    added flutter frontend

--- backend_python/api/urls.py
+++ backend_python/api/urls.py
@@ -1,5 +1,7 @@
 from django.urls import path
 
+from api.views.user_views import UserRecordView
+
 urlpatterns = [
-    #path('user/', UserRecordView.as_view(), name='users'),
+    path('user/', UserRecordView.as_view(), name='users'),
 ]
